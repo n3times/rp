6df146348fe6169762d32784672f6f24584f436a
Paul Novaes <n3times@users.noreply.github.com>
2026-04-04 10:11:04 -0700

    When zooming, render from the center to the edge of the screen

--- mandelbrot_viewer.py
+++ mandelbrot_viewer.py
@@ -2,7 +2,7 @@ import pygame, sys
 
 W, H = 800, 600
 
-INITIAL_CENTER = 0 + 0j
+INITIAL_CENTER = -0.5 + 0j
 INITIAL_ZOOM = 1
 INITIAL_MAX_ITER = 128
 
@@ -13,23 +13,29 @@ def escape_time(c, max_iter):
         if abs(z) > 2: return i
     return max_iter
 
-def translate(screen_pos, center, zoom):
-    i, j = screen_pos
+def pixel_to_complex(pixel, center, zoom):
+    i, j = pixel
     j = H - j
     x = center.real - (W // 2 - i) / (W // 2) / zoom * 2
     y = center.imag - (H // 2 - j) / (H // 2) / zoom * 2 * H / W
     return complex(x, y)
 
-def get_color(escape_time, max_iter):
-    # Black for the Mandelbrot set and white to mid gray outside
-    val = 0 if escape_time == max_iter else 128 + escape_time%128
+def color_for_iter(num_iter, max_iter):
+    val = 0 if num_iter == max_iter else 128 + num_iter % 128
     return (val, val, val)
 
-def render_column(surface, col, center, zoom, max_iter):
-    for row in range(H):
-        c = translate((col, row), center, zoom)
-        time = escape_time(c, max_iter)
-        surface.set_at((col, row), get_color(time, max_iter))
+def render_pixel(surface, row, col, center, zoom, max_iter):
+    c = pixel_to_complex((col, row), center, zoom)
+    num_iter = escape_time(c, max_iter)
+    surface.set_at((col, row), color_for_iter(num_iter, max_iter))
+
+def render_ring(surface, ring, center, zoom, max_iter):
+    for row in (H // 2 - ring, H // 2 + ring):
+        for col in range(W // 2 - ring, W // 2 + ring + 1): 
+            render_pixel(surface, row, col, center, zoom, max_iter)
+    for row in range(H // 2 - ring, H // 2 + ring + 1):
+        for col in (W // 2 - ring, W // 2 + ring): 
+            render_pixel(surface, row, col, center, zoom, max_iter)
 
 pygame.init()
 screen = pygame.display.set_mode((W, H))
@@ -38,7 +44,7 @@ clock = pygame.time.Clock()
 center = INITIAL_CENTER
 max_iter = INITIAL_MAX_ITER
 zoom = INITIAL_ZOOM
-next_col = 0
+next_ring = 0
 
 while True:
     for e in pygame.event.get():
@@ -46,12 +52,12 @@ while True:
             pygame.quit()
             sys.exit()
         if e.type == pygame.MOUSEBUTTONDOWN:
-            center = translate(e.pos, center, zoom)
+            center = pixel_to_complex(e.pos, center, zoom)
             if e.button == 3:  # right click
                 zoom = max(1, zoom // 2)
             else:
                 zoom *= 2
-            next_col = 0
+            next_ring = 0
         elif e.type == pygame.KEYDOWN:
             # Use brackets to change the "resolution" (max_iter)
             if e.key == pygame.K_LEFTBRACKET:
@@ -60,32 +66,32 @@ while True:
                 max_iter *= 2
             # Use arrows to pan half a screen in one direction
             elif e.key == pygame.K_LEFT:
-                center = translate((0, H//2), center, zoom)
+                center = pixel_to_complex((0, H//2), center, zoom)
             elif e.key == pygame.K_RIGHT:
-                center = translate((W, H//2), center, zoom)
+                center = pixel_to_complex((W, H//2), center, zoom)
             elif e.key == pygame.K_DOWN:
-                center = translate((W//2, H), center, zoom)
+                center = pixel_to_complex((W//2, H), center, zoom)
             elif e.key == pygame.K_UP:
-                center = translate((W//2, 0), center, zoom)
+                center = pixel_to_complex((W//2, 0), center, zoom)
             # Use "R" to reset
             elif e.key == pygame.K_r:
                 center = INITIAL_CENTER
-                zoom = 1
-                max_iter = 128
-            next_col = 0
+                zoom = INITIAL_ZOOM
+                max_iter = INITIAL_MAX_ITER
+            next_ring = 0
 
     # Prepare to draw
-    if next_col == 0:
+    if next_ring == 0:
         screen.fill((255, 255, 255))
         pygame.display.set_caption(
             f"Zoom {zoom:,} -- Iters {max_iter:,}"
         )
 
-    # Draw a few columns at a time
-    if next_col < W:
-        for col in range(next_col, min(next_col + 3, W)):
-            render_column(screen, col, center, zoom, max_iter)
-        next_col += 3
+    # Draw a few rings at a time
+    if next_ring <= max(W, H) // 2:
+        for radius in range(next_ring, next_ring + 3):
+            render_ring(screen, radius, center, zoom, max_iter)
+        next_ring += 3
 
     pygame.display.flip()
     clock.tick(60)
